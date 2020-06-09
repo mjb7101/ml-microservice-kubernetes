6deae33c242888c285ce25c461043448297ceb91
Mike Bowles <mike.bowles@proquest.com>
2020-06-09 16:07:54 +0100

    adding kubernetes deployment steps

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=dmjb7101/flaskapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flaskapp-pod\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward flaskapp-pod 8000:80
 
