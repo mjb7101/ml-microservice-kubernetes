fe3dfe05999d949a049482d9802bdfa2694c63e7
Mike Bowles <mike.bowles@proquest.com>
2020-06-09 15:38:35 +0100

    logging the output prediction value

--- app.py
+++ app.py
@@ -63,7 +63,10 @@ def predict():
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
+    
+    # Log the output prediction value
+    LOG.info(f"Output prediction values are: {prediction}")
+
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
